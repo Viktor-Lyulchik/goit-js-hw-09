584365576b47e3d89a0fca2268d9eaf2b0ede978
Viktor Liulchyk <viktor.lyulchik@gmail.com>
2025-05-16 00:10:16 +0300

    solved problems in task2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,32 +1,37 @@
-const form = document.querySelector('.feedback-form');
+let formData = { email: '', message: '' };
+
+const formFeedBack = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
-let formData = {};
 
-// Відновлення даних з localStorage при завантаженні сторінки
-document.addEventListener('DOMContentLoaded', () => {
-  const savedData = localStorage.getItem(STORAGE_KEY);
-  if (savedData) {
-    try {
-      formData = JSON.parse(savedData);
-      if (formData.email) form.elements.email.value = formData.email;
-      if (formData.message) form.elements.message.value = formData.message;
-    } catch (error) {
-      console.error('Помилка при парсингу даних з localStorage:', error);
-    }
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  try {
+    formData = JSON.parse(savedData);
+    if (formData.email) formFeedBack.elements.email.value = formData.email;
+    if (formData.message)
+      formFeedBack.elements.message.value = formData.message;
+  } catch (error) {
+    console.error('Помилка при парсингу даних з localStorage:', error);
   }
-});
+}
 
-// Збереження даних у localStorage при введенні користувача
-form.addEventListener('input', event => {
-  formData[event.target.name] = event.target.value;
+formFeedBack.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value.trim();
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 });
 
-// Очищення localStorage та форми при відправці
-form.addEventListener('submit', event => {
+formFeedBack.addEventListener('submit', event => {
   event.preventDefault();
-  console.log('Дані форми:', formData);
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  } else {
+    console.log('Дані форми:', formData);
+  }
   localStorage.removeItem(STORAGE_KEY);
-  form.reset();
-  formData = {};
+
+  formData = { email: '', message: '' };
+
+  formFeedBack.reset();
 });
